aea0d4b7e3bb4520a292437804b0079a313e3051
Daniel Santana <github@danielsantana.net>
2017-04-24 00:33:23 -0400

    unifies bunyan and pino APIs on how to set names

--- logr.js
+++ logr.js
@@ -2,8 +2,21 @@
 
 function getBunyan(level) {
   const bunyan = require('bunyan');
+  
+  bunyan.prototype.child = function (options, simple) {
+    var name;
+    if (options && options.name) {
+      name = options.name;delete options.name;
+    }
 
-  return bunyan.createLogger({
+    const child = new (this.constructor)(this, options || {}, simple);
+
+    if (name) child.fields.name = name;
+
+    return child;
+  };
+
+  const logr = bunyan.createLogger({
     name: 'BUNY',
     level,
     serializers: {
@@ -11,11 +24,24 @@ function getBunyan(level) {
       res: bunyan.stdSerializers.res
     },
     src: ( ['debug','trace'].indexOf(level) !== -1 ),
-  })
+  });
+
+  return Object.defineProperty(logr, 'name', {
+    get( ){ return this.fields.name; },
+    set(_){ return this.fields.name = _; }
+  });
 }
 
 function getPino(level) {
-  return require('pino')({name: 'PINO', level});
+  const pino = require('pino')({name: 'PINO', level});
+
+  pino.fields = {pino};
+  Object.defineProperty(pino.fields, 'name', {
+    get( ){ return this.pino.name; },
+    set(_){ return this.pino.name = _; }
+  });
+
+  return pino;
 }
 
 const level  = !process.env.LOG_LEVEL ? 'debug' :
